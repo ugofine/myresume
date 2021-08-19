d82fa7019fbe5b4506f20ef1b03cdf94e19f48b8
Debbie <62076688+ugofine@users.noreply.github.com>
2021-08-19 16:38:10 +0100

    modifed the  models and views

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from portfolio.models import Port
 
 # Create your views here.
+def home(request):
+    portfolio = Port.objects.all()
+        
+    return render(request, 'pages/index.html', {'portfolio' : portfolio})
